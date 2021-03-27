fbb081db754a10dc8b7706d792c9f2521431930d
Gweix0 <81273333+Gweix0@users.noreply.github.com>
2021-03-27 15:11:48 +0800

    打印1000-2000的闰年

---
+++ dsa.c
@@ -0,0 +1,15 @@
+int main()			//打印1000-2000内的闰年
+{
+	int year = 0;
+	int count = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+		{
+			printf("%d ", year);
+			count++;
+		}
+	}
+	printf("\ncount=%d", count);
+	return 0;
+}
